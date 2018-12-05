5ea4764f1d4585a139cc1399603e1aad958103eb
Chris Vickery <chrisinajar@gmail.com>
2018-12-04 23:42:18 -0800

    Updates

---
+++ precache-manifest.bf7f35da69503680eaf02c3c3531b4e1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1217cd4e3950b3818d3586d9b202b55",
+    "url": "/disastles-online/static/media/BG5.b1217cd4.png"
+  },
+  {
+    "revision": "619dd64dcf0889fee00342c08eed55c1",
+    "url": "/disastles-online/static/media/BG4.619dd64d.png"
+  },
+  {
+    "revision": "bb846d3d2f60985b280383eeb436b497",
+    "url": "/disastles-online/static/media/BG3.bb846d3d.png"
+  },
+  {
+    "revision": "4ca3ca0db7de5e5a8fafb7083634d2f5",
+    "url": "/disastles-online/static/media/BG2.4ca3ca0d.png"
+  },
+  {
+    "revision": "5b931fc68692e554143380117c497d45",
+    "url": "/disastles-online/static/media/BG1.5b931fc6.png"
+  },
+  {
+    "revision": "a07426da96600ea0ec0d",
+    "url": "/disastles-online/static/js/runtime~main.a07426da.js"
+  },
+  {
+    "revision": "8c2e33abe376b90ec3f5",
+    "url": "/disastles-online/static/js/main.8c2e33ab.chunk.js"
+  },
+  {
+    "revision": "9dbb1b50113a99c12aa5",
+    "url": "/disastles-online/static/js/1.9dbb1b50.chunk.js"
+  },
+  {
+    "revision": "8c2e33abe376b90ec3f5",
+    "url": "/disastles-online/static/css/main.3b023798.chunk.css"
+  },
+  {
+    "revision": "eb785536c29f3b191ec4bb6683937114",
+    "url": "/disastles-online/index.html"
+  }
+];
