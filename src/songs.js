31f759f850e89ecefc78e7cd476253f294b28e66
Chris Vickery <chrisinajar@gmail.com>
2019-01-28 17:46:18 -0800

    Add last song and fix song metadata

--- src/songs.js
+++ src/songs.js
@@ -1,12 +1,15 @@
+/*
+
+*/
 const songs = [{
-  title: 'Plug Play - Take me Back',
+  title: 'Take me Back',
   author: 'Betamaxx',
   file: '01 Betamaxx - Plug Play - Take me Back.mp3',
   length: 238,
 }, {
   file: '02 Phaserland - Night Talk in Paradise - Magenta Eyes.mp3',
   author: 'Phaserland',
-  title: 'Night Talk in Paradise - Magenta Eyes',
+  title: 'Magenta Eyes',
   length: 253,
 }, {
   file: '03 Kilian Oser - Disastles.mp3',
@@ -15,12 +18,12 @@ const songs = [{
   length: 154,
 }, {
   file: '04 LIFE (Disastles Remaster).mp3',
-  author: 'Kilian Oser',
-  title: 'LIFE (Disastles Remaster)',
+  author: 'Favorit89',
+  title: 'Life (Disastles Remaster)',
   length: 263,
 }, {
   file: '05 Catalogue [Remaster].mp3',
-  author: 'Catalogue',
+  author: 'Baldocaster',
   title: 'Catalogue [Remaster]',
   length: 216,
 }, {
@@ -41,7 +44,7 @@ const songs = [{
 }, {
   file: '09 Damokles - This Time we made it through.mp3',
   author: 'Damokles',
-  title: 'This Time we made it through',
+  title: 'This Time We Made It Through',
   length: 281,
 }, {
   file: '10 Dream Fiend - Electric Isle.mp3',
@@ -55,19 +58,24 @@ const songs = [{
   length: 233,
 }, {
   file: '12 Flourish.mp3',
-  author: 'Flourish',
+  author: 'Bart Graft',
   title: 'Flourish',
   length: 255,
 }, {
   file: '13 Arcis Speed Boat Theme.mp3',
-  author: 'Arcis Speed Boat Theme',
-  title: 'Arcis Speed Boat Theme',
+  author: 'Arcis',
+  title: 'Speed Boat Theme',
   length: 220,
 }, {
   file: '14 Naeon 247.mp3',
   author: 'Naeon',
-  title: '247',
+  title: '24:7',
   length: 291,
+}, {
+  file: '15 80s_Stallone - Party at Kitty and Studs.mp3',
+  author: '80s Stallone',
+  title: 'Party at Kitty and Studs',
+  length: 258,
 }];
 
 export default songs;
