c3f4eedbe3871397648c294ce342f8233ab53a11
Chris Vickery <chrisinajar@gmail.com>
2019-01-29 22:04:10 -0800

    Back to heroku, fix swapping rooms

--- src/api/ws.js
+++ src/api/ws.js
@@ -1,7 +1,7 @@
 import Event from 'geval/event';
 
 const MessageEvent = Event();
-const url = 'ws://34.246.160.198/api/ws';
+const url = 'wss://disastles.herokuapp.com/api/ws';
 
 var ws = null;
 var initPromise = null;
